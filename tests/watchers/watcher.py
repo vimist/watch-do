2aef2e3bd67dae12d14aa0bb1c26e2257fac253d
vimist <vimist@users.noreply.github.com>
2017-02-05 09:55:46 +0000

    Added base `Watcher` class and tests

---
+++ tests/watchers/watcher.py
@@ -0,0 +1,40 @@
+"""Test the base `Watcher` class
+"""
+
+from unittest import TestCase
+from unittest.mock import patch
+import tempfile
+
+from watch_do.watchers import Watcher
+
+
+class TestWatcher(TestCase):
+    """Test the base `Watcher` class
+    """
+
+    def setUp(self):
+        """Create an instance of the watcher referencing a temporary file.
+        """
+        self.temporary_file = tempfile.NamedTemporaryFile('r')
+        self.watcher = Watcher(self.temporary_file.name)
+
+    def test_init(self):
+        """Check the temporary file's name has been stored correctly.
+        """
+        self.assertEqual(self.watcher.file_name, self.temporary_file.name)
+
+    def test_has_changed(self):
+        """Check the method recognises changes in values.
+        """
+        # Patch the _get_value to return 'Hello'
+        with patch('watch_do.watchers.Watcher._get_value') as get_value:
+            get_value.return_value = 'Hello'
+
+            self.assertTrue(self.watcher.has_changed())
+            # Nothing changed, so the result should be False now
+            self.assertFalse(self.watcher.has_changed())
+
+    def test__get_value(self):
+        """Check this method isn't implemented in the base class.
+        """
+        self.assertRaises(NotImplementedError, self.watcher._get_value)
---
+++ watch_do/watchers/watcher.py
@@ -0,0 +1,52 @@
+"""The base `Watcher`.
+"""
+
+class Watcher:
+    """The base `Watcher` that all other watchers should inherit from.
+
+    This class enables child classes to concentrate on the change detection
+    and to not have to concern themselves about how this is presented to the
+    user.
+    """
+
+    def __init__(self, file_name):
+        self._file_name = file_name
+        self._last_value = None
+
+    @property
+    def file_name(self):
+        """Get the file name that this watcher is watching.
+        """
+        return self._file_name
+
+    def has_changed(self):
+        """Determine if the file has changed.
+
+        Determine if the file has changed singe the last time this
+        method was called.
+
+        Returns:
+            bool: A boolean, indicating if the watched file has changed.
+        """
+        value = self._get_value()
+
+        changed = True if value != self._last_value else False
+
+        self._last_value = value
+        return changed
+
+    def _get_value(self):
+        """Get the current value of the watched file.
+
+        **Should be overwritten and implemented in child classes.**
+
+        Determines the current change value of the file being watched. This
+        could be the file's hash, the modified time, or some other value that
+        can be used to determine if we should update the `changed` status.
+
+        Returns:
+            str: A value representing the current state of the object that this
+            base class can use to determine if the file has changed.
+        """
+        raise NotImplementedError(
+            'The `_get_value` method should be implemented by the child class')
