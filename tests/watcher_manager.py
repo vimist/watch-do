fb3ccf66b853f6dec721ba43e9986308e51efd5d
vimist <vimist@users.noreply.github.com>
2017-02-08 21:29:07 +0000

    Added WatcherManager and tests

---
+++ tests/watcher_manager.py
@@ -0,0 +1,81 @@
+from unittest import TestCase
+from unittest.mock import patch
+import tempfile
+import os
+
+from tests.helper_functions import set_up_test_files
+from tests.helper_functions import create_file
+from tests.helper_functions import remove_file
+
+from watch_do import WatcherManager
+from watch_do import GlobManager
+from watch_do.watchers import Watcher
+from watch_do.watchers.hash import MD5
+
+
+class TestWatcherManager(TestCase):
+    def setUp(self):
+        """Create some temporary files for the tests to work with.
+        """
+        self.temp_dir = tempfile.TemporaryDirectory()
+        set_up_test_files(self.temp_dir.name)
+
+        self.glob_manager = GlobManager(['*'])
+        self.watcher_manager = WatcherManager(
+            MD5, self.glob_manager, True, True)
+
+        # Change to the temporary directory
+        self.cwd = os.getcwd()
+        os.chdir(self.temp_dir.name)
+
+    def tearDown(self):
+        """Remove the temporary directory and files.
+        """
+        self.temp_dir.cleanup()
+        os.chdir(self.cwd)
+
+    def test___init__(self):
+        """Check that all passed in properties are being stored correctly.
+        """
+        self.assertTrue(issubclass(self.watcher_manager.watcher, Watcher))
+        self.assertIsInstance(self.watcher_manager.glob_manager, GlobManager)
+        self.assertTrue(self.watcher_manager.reglob)
+        self.assertTrue(self.watcher_manager.changed_on_remove)
+
+    def test_get_changed_files(self):
+        """Chack that new, removed and changed files are being reported.
+        """
+        # No changed files to start with
+        self.assertEqual(self.watcher_manager.get_changed_files(), set(''))
+
+        # New file
+        create_file('something_random.jpeg')
+        self.assertEqual(self.watcher_manager.get_changed_files(),
+                         {'something_random.jpeg'})
+
+        # Removed file (as `changed_on_remove` is True)
+        remove_file('something_random.jpeg')
+        self.assertEqual(self.watcher_manager.get_changed_files(),
+                         {'something_random.jpeg'})
+
+        # Change file
+        create_file('dave.txt', 'Hello World')
+        self.assertEqual(self.watcher_manager.get_changed_files(),
+                         {'dave.txt'})
+
+        # Disable changed_on_remove
+        self.watcher_manager._changed_on_remove = False
+        remove_file('dave.txt')
+        self.assertEqual(self.watcher_manager.get_changed_files(),
+                         set())
+
+        # New file with reglob disabled
+        self.watcher_manager._reglob = False
+        create_file('dave.txt')
+        self.assertEqual(self.watcher_manager.get_changed_files(),
+                         set())
+
+        # Removed file with reglob disabled
+        remove_file('bob.py')
+        self.assertEqual(self.watcher_manager.get_changed_files(),
+                         set())
---
+++ watch_do/watcher_manager.py
@@ -0,0 +1,112 @@
+"""The `WatcherManager` is responsible for creating and deleting watchers.
+"""
+
+
+class WatcherManager:
+    """The `WatcherManager` is responsible for creating and deleting watchers.
+    """
+
+    def __init__(self, watcher, glob_manager, reglob, changed_on_remove):
+        """Initialise the `WatcherManager` properties.
+
+        Properties:
+            watcher (...): A reference to a subclass of `Watcher` that will be
+                           used watch the files provided by the `GlobManager`.
+            glob_manager (GlobManager): The glob manager responsible for
+                                        providing a list of files.
+            reglob (bool): A boolean value indicating whether to re-evaluate
+                           globs when `changed_files` is called.
+            changed_on_remove (bool): A boolean value indicating whether to
+                                      consider the removal of a file a change.
+        """
+        self._watcher = watcher
+        self._glob_manager = glob_manager
+        self._reglob = reglob
+        self._changed_on_remove = changed_on_remove
+
+        self._first_call_to_changed_files = True
+        self._files = set()
+        self._watchers = {}
+
+    @property
+    def watcher(self):
+        """Get a reference to the watcher class that is being used.
+        """
+        return self._watcher
+
+    @property
+    def glob_manager(self):
+        """Get the `GlobManager` instance that's providing file names to watch.
+        """
+        return self._glob_manager
+
+    @property
+    def reglob(self):
+        """Get the value determining whether we are re-evaluating file globs.
+        """
+        return self._reglob
+
+    @property
+    def changed_on_remove(self):
+        """Get the value determining whether removed files count as a change.
+        """
+        return self._changed_on_remove
+
+    def get_changed_files(self):
+        """Get a `set` of changed files.
+
+        This method determines which files have changed since the last time
+        this method was called. Added files, changed files (determined by the
+        type of watcher) and removed files (if `changed_on_remove` is True) are
+        all counted as changed files.
+
+        The watchers are stored and managed internally to this class.
+
+        Returns:
+            set: A `set` of files that have changed since the last time this
+                 method was called.
+        """
+        # Keep track of added and removed files
+        added_files = set()
+        removed_files = set()
+
+        # Update the list of files we should be watching. Only do this if
+        # this is the first time, or if reglob is set to True
+        if self.reglob or self._first_call_to_changed_files:
+            new_files = self.glob_manager.get_files()
+
+            added_files = new_files - self._files
+            removed_files = self._files - new_files
+
+            self._files = new_files
+
+        changed_files = set()
+
+        # Create watchers for newly found files
+        for file_name in added_files:
+            self._watchers[file_name] = self.watcher(file_name)
+
+            if not self._first_call_to_changed_files:
+                changed_files.add(file_name)
+
+        # Remove watchers for non existant files
+        for file_name in removed_files:
+            del self._watchers[file_name]
+
+            if self.changed_on_remove:
+                changed_files.add(file_name)
+
+        # Check for changed files
+        for watcher in self._watchers:
+            try:
+                if self._watchers[watcher].has_changed():
+                    changed_files.add(self._watchers[watcher].file_name)
+            except FileNotFoundError as ex:
+                # Handle FileNotFoundError exceptions only if reglobbing is
+                # False, otherwise re-raise it
+                if self.reglob:
+                    raise ex
+
+        self._first_call_to_changed_files = False
+
+        return changed_files
